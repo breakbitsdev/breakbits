40db1f30bdba1c9ed621dd3c1d6bd5ee71143c56
breakbitsdev <aligdev@mail.com>
2018-06-27 12:55:32 +0800

    Second Release

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BREAKBITS-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BREAKBITSURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
